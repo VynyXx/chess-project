118868cc8f9f5b783927875190423c4e83faaa42
Vynyx <vinicius.pereiras@outlook.com>
2021-07-26 08:01:25 -0300

    First class: Position

---
+++ src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Position p = new Position(4, 2);
+		
+		System.out.println(p);
+
+	}
+}
